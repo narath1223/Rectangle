9bb00a4f6be4d64a3d77cfd3158d83a9ea300bf3
narath1223 <156156842+narath1223@users.noreply.github.com>
2024-01-11 08:46:27 +0700

    Add files via upload

---
+++ Test2/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+    public static void main(String[] args) {
+        // Example usage
+
+        Square sq = new Square(4);
+        System.out.println("Square Area: " + sq.getArea());
+        System.out.println("Square Perimeter: " + sq.getPerimeter());
+        Rectangle rectangle = new Rectangle(3, 5);
+
+
+        System.out.println("Rectangle Area: " + rectangle.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
+
+    }
+}
